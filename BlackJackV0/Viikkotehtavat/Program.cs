6bb85e0f02ef61da2e268c9fdbc61688f4fe7467
K2542 <K2542@ICT-421-WS-22>
2017-01-17 17:10:16 +0200

    Tehtävä 1 tehty

---
+++ BlackJackV0/Viikkotehtavat/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tehtava1();
+        }
+
+    static void Tehtava1()
+        {
+            // Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme). Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku".
+            Console.WriteLine("Anna 1,2 tai 3 ja tulostan sen tekstinä");
+            string retval = Console.ReadLine();
+            int luku = int.Parse(retval);
+            if (luku == 1)
+            {
+                retval = "Yksi";
+            }
+            else if (luku == 2)
+            {
+                retval = "Kaksi";
+            }
+            else if (luku == 3)
+            {
+                retval = "Kolme";
+            }
+            else
+            {
+                retval = "Joku muu luku";
+            }
+            //tulostus käyttäjälle
+            Console.WriteLine(retval);
+        }
+    static void Tehtava2()
+        {
+            //Tee ohjelma, jossa annetaan oppilaalle koulunumero seuraavan taulukon mukaan (pistemäärä kysytään ja ohjelma tulostaa numeron)
+            Console.WriteLine("Anna pistemäärä ja tulostan numeron");
+            string score = Console.ReadLine();
+            
+        }
+    }
+
+}
